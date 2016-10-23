d4e1fbc492802516d67c8ade5bc944c1161b2a57
maheshbhudia <mahesh.bhudia@kcl.ac.uk>
2016-10-23 12:45:02 +0100

    Working basic start to the main page

--- app/src/main/java/com/example/maheshuk31/killerrobotapplication/MainActivity.java
+++ app/src/main/java/com/example/maheshuk31/killerrobotapplication/MainActivity.java
@@ -1,22 +1,26 @@
 package com.example.maheshuk31.killerrobotapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgRobot;
+    private TextView txtIntroduction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        imgRobot = (ImageView) findViewById(R.id.imgRobot);
+        txtIntroduction = (TextView) findViewById(R.id.txtIntroduction);
 
 
     }
