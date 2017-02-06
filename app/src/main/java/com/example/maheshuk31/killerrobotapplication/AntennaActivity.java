1d77f835e554db917eb1513845edeecd2611f4f8
maheshbhudia <mahesh.bhudia@kcl.ac.uk>
2017-02-06 19:54:26 +0000

    Redone the colours for texts to match, started to work on the antenna page

--- app/src/main/java/com/example/maheshuk31/killerrobotapplication/AntennaActivity.java
+++ app/src/main/java/com/example/maheshuk31/killerrobotapplication/AntennaActivity.java
@@ -1,18 +1,26 @@
 package com.example.maheshuk31.killerrobotapplication;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class AntennaActivity extends AppCompatActivity {
+
+    private TextView txtAntennaTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_antenna);
+
+//        txtAntennaTitle = (TextView) findViewById(R.id.txtAntennaTitle);
+//        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/disposabledroidbb.ttf");
+//        txtAntennaTitle.setTypeface(typeFace);
     }
 
 }
