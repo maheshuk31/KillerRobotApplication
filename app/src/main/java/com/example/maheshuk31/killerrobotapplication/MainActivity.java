dfac1446384139a31d81f9a38e6028a0d3c72b45
maheshbhudia <mahesh.bhudia@kcl.ac.uk>
2017-03-12 19:30:59 +0000

    Removed unnecessary import statements

--- app/src/main/java/com/example/maheshuk31/killerrobotapplication/MainActivity.java
+++ app/src/main/java/com/example/maheshuk31/killerrobotapplication/MainActivity.java
@@ -2,16 +2,9 @@ package com.example.maheshuk31.killerrobotapplication;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
